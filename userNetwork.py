feb038d92dcecfd9519a849708108cf137e971a9
Stefan Krug <stefan.krug@posteo.de>
2020-04-15 18:35:41 +0200

    update

--- userNetwork.py
+++ userNetwork.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3 
 import os.path
 import requests
+import networkx as nx
+import matplotlib.pyplot as plt
 from lxml import etree
 from urllib.parse import urlparse
 from pyvis.network import Network
@@ -51,18 +53,17 @@ def addArticleData(nodes, edges, article):
     # article hinzufügen, sofern neu
     if article_node not in nodes:
         nodes.append(article_node)
+        print("article added: " + str(article_node))
     # user als nodes, versions als edges hinzufügen
     for version in article.xpath('/article/versions/version'):
         # user-node zusammenstellen
         user_node = {"name": version.xpath('./user')[0].text
                      , "lang": article_node["lang"] # bessere quelle haben wir aktuell nicht
                      , "type": "user"}
-        #LOG
-        print(user_node)
-        if user_node["name"] == "DittyReacts": print(80*"-")
         # user hinzufügen, sofern neu
         if user_node not in nodes:
             nodes.append(user_node)
+            print("user added: " + str(user_node))
         # version als edge hinzufügen
         version_edge = {"user": user_node["name"]
                         , "article": article_node["name"]
@@ -81,6 +82,7 @@ def addUserData(nodes, edges, user):
     # user hinzufügen, sofern neu
     if user_node not in nodes:
         nodes.append(user_node)
+        print("user added: " + str(user_node))
     # articles als nodes, versions als edges hinzufügen
     if user.xpath('/user/versions/version') is not None:
         for version in user.xpath('/user/versions/version'):
@@ -91,6 +93,7 @@ def addUserData(nodes, edges, user):
             # article hinzufügen, sofern neu
             if article_node not in nodes:
                 nodes.append(article_node)
+                print("article added: " + str(article_node))
             # version als edge hinzufügen
             version_edge = {"user": user_node["name"]
                             , "article": article_node["name"]
@@ -100,55 +103,88 @@ def addUserData(nodes, edges, user):
                 edges.append(version_edge)
         
 # =============================================================================    
+# Visualisierung über Pyvis, browserbasiert, physics
+                
+def createPyvisNetwork(nodes, edges):    
+    ## Graph anlegen        
+    netGraph = Network(height='750pt', width='100%', bgcolor="#222222", font_color="white")        
+    # Daten in Graph eintragen                   
+    for node in nodes:
+        if node["type"] == "user":
+            netGraph.add_node(node["name"], shape="dot")
+        elif node["type"] == "article":
+            if node["lang"] == "zh":
+                netGraph.add_node(node["name"], shape="star", title = "https://"+node["lang"]+".wikipedia.org/"+node["name"], color = "red")
+            else:
+                netGraph.add_node(node["name"], shape="star", title = "https://"+node["lang"]+".wikipedia.org/"+node["name"], color = "blue")
+    for edge in edges:
+        netGraph.add_edge(edge["user"], edge["article"])
+    #netGraph.barnes_hut()
+    netGraph.show('networkmap.html')        
+
+# =============================================================================  
+# Visualisierung über Networkx, minimalistisch
+    
+def createNetxNetwork(nodes, edges):  
+    graph = nx.Graph()
+    plt.rcParams["figure.figsize"] = (30, 30)
+    
+    for node in nodes:
+        graph.add_node(node["name"])
+    for edge in edges:
+        graph.add_edge(edge["user"], edge["article"])
+#    
+#    # User als nodes hinzufügen
+#    for user in history.xpath('/article/versions/version/user'):
+#        graph.add_node(user.text)
+#    # Versionen als edges hinzufügen
+#    for version in history.xpath('/article/versions/version'):
+#        graph.add_edge(title, version.xpath('./user')[0].text, timestamp=version.xpath('./user')[0].text)
+#    
+    nx.draw_kamada_kawai(graph, with_labels=True, node_size=300)
+    nx.draw_networkx(graph)
+    
+    
+    plt.savefig("graph.png")
+    plt.show()
+
+# =============================================================================    
         
+def createGephiExport(nodes, edges):
+    with open('nodes.csv', 'w', newline='') as csvfile:
+        fieldnames = ['name', 'lang', 'type']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(nodes)
+    with open('edges.csv', 'w', newline='') as csvfile:
+        fieldnames = ['user', 'article', 'timestamp', 'id']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(edges)
+
+# =============================================================================    
+
 nodes = list()
 edges = list()
 
+# Articles Abrufen und als XML speichern. NB Limit anpassen
 # en article
-addArticleData(nodes, edges, getXMLdata('https://en.wikipedia.org/w/index.php?title=1989_Tiananmen_Square_protests&action=history&limit=100', 'history.xsl'))
-#addArticleData(nodes, edges, getXMLdata('https://en.wikipedia.org/w/index.php?title=Hong_Kong&action=history&limit=100', 'history.xsl'))
-
+addArticleData(nodes, edges, getXMLdata('https://en.wikipedia.org/w/index.php?title=1989_Tiananmen_Square_protests&action=history&limit=500', 'history.xsl'))
+addArticleData(nodes, edges, getXMLdata('https://en.wikipedia.org/w/index.php?title=Hong_Kong&action=history&limit=500', 'history.xsl'))
 # zh article
-addArticleData(nodes, edges, getXMLdata('https://zh.wikipedia.org/w/index.php?title=%E5%85%AD%E5%9B%9B%E4%BA%8B%E4%BB%B6&action=history&limit=100', 'history.xsl'))
+addArticleData(nodes, edges, getXMLdata('https://zh.wikipedia.org/w/index.php?title=%E5%85%AD%E5%9B%9B%E4%BA%8B%E4%BB%B6&action=history&limit=500', 'history.xsl'))
+addArticleData(nodes, edges, getXMLdata('https://zh.wikipedia.org/w/index.php?title=%E9%A6%99%E6%B8%AF&action=history&limit=500', 'history.xsl'))
 
 # zugehörige user-netzwerke ermitteln
 for node in nodes:
     if node["type"] == "user":
-        print("ermittele Daten für User: "+node["name"])
+        # Aufruf je Sprachversion, NB &target=USERNAME muss als letztes Element notiert sein (sonst liefert das Schema nichts)
         addUserData(nodes, edges, getXMLdata('https://en.wikipedia.org/w/index.php?title=Special:Contributions&limit=100&target=' + node["name"], 'user.xsl'))
-        #addUserData(nodes, edges, getXMLdata('https://zh.wikipedia.org/wiki/Special:用户贡献/' + node["name"] + '&limit=100', 'user.xsl'))
         addUserData(nodes, edges, getXMLdata('https://zh.wikipedia.org/w/index.php?title=Special:用户贡献&limit=100&target=' + node["name"], 'user.xsl'))
         
-## als csv speichern - für gephi
-#with open('nodes.csv', 'w', newline='') as csvfile:
-#    fieldnames = ['name', 'lang', 'type']
-#    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#    writer.writeheader()
-#    writer.writerows(nodes)
-#with open('edges.csv', 'w', newline='') as csvfile:
-#    fieldnames = ['user', 'article', 'timestamp', 'id']
-#    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#    writer.writeheader()
-#    writer.writerows(edges)
-
-## Graph anlegen        
-netGraph = Network(height='750pt', width='100%', bgcolor="#222222", font_color="white")        
-
-# Daten in Graph eintragen                   
-for node in nodes:
-    if node["type"] == "user":
-        netGraph.add_node(node["name"], shape="dot")
-    elif node["type"] == "article":
-        if node["lang"] == "zh":
-            netGraph.add_node(node["name"], shape="database", title = "https://zh.wikipedia.org", color = "red")
-        else:
-            netGraph.add_node(node["name"], shape="database", title = "https://zh.wikipedia.org", color = "blue")
-for edge in edges:
-    netGraph.add_edge(edge["user"], edge["article"])
-
-netGraph.barnes_hut()
-netGraph.show('networkmap.html')
-
+#createNetxNetwork(nodes, edges)
+#createGephiExport(nodes, edges)
+createPyvisNetwork(nodes, edges)
 
 
 # ================== Stuff ================================
